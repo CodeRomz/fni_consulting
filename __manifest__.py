2ad52ac71da2d2a992ae3eff73a864371ab56f55
CyberRoms <romualdojrb@yahoo.com>
2025-08-05 13:19:45 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -2,15 +2,21 @@
     'name': 'FNI Consulting Customizations',
     'version': '17.0.1.0.0',
     'summary': 'Custom patches and business logic for FNI Consulting',
-    'author': 'CodeROmz',
+
+    'author': 'CodeRomz',
+    'website': "https://github.com/CodeRomz",
+    'license': 'LGPL-3',
+
     'category': 'Custom',
-    'depends': ['hr_timesheet', 'project'],
+    'depends': ['hr_timesheet_sheet'],
+
     'data': [
         'security/ir.model.access.csv',
         'views/task_type_view.xml',
         'views/account_analytic_line_view.xml',
+        'views/hr_timesheet_sheet_readonly.xml',
     ],
     'installable': True,
     'application': False,
-    'license': 'LGPL-3',
+
 }
