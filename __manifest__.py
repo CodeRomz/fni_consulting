e90472e64fca8c1640e43e5509da8ba572a92115
CyberRoms <romualdojrb@yahoo.com>
2025-09-18 10:05:19 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -4,7 +4,7 @@
 
     'author': 'CodeRomz',
     'website': "https://github.com/CodeRomz",
-    'license': 'AGLP-3',
+    'license': 'LGPL-3',
     'version': '17.0.1.0.0',
 
     'category': 'Custom',
