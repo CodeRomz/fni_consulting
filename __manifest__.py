fdd200d2bb6729306b8f5816445f659f87a14e7d
CodeRomz <romualdojrb@yahoo.com>
2025-11-14 05:35:52 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -18,7 +18,8 @@
         'views/res_partner_bank.xml',
         'reports/report_invoice_fni_template.xml',
         'reports/invoice_report_action.xml',
-        'reports/external_layout_striped.xml',
+        'reports/external_layout_striped_header.xml',
+        'reports/external_layout_striped_footer.xml',
 
     ],
 
