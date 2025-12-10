d9dc75ec4bdc038f2bf1b591998ba5ebbb9a8ac4
CodeRomz <romualdojrb@yahoo.com>
2025-12-10 16:12:54 +0400

    .

--- __manifest__.py
+++ __manifest__.py
@@ -14,6 +14,7 @@
         'security/ir.model.access.csv',
         'views/res_partner_bank.xml',
         'reports/invoice_report_fni_template.xml',
+        'reports/survey_report_fni_acknowledgement_template.xml',
         'reports/invoice_report_action.xml',
         'reports/external_layout_striped_header.xml',
         'reports/external_layout_striped_footer.xml',
